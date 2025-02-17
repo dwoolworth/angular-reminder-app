3c7444c3691e126fd862624773d3c238eda56422
Edin Omeragic <edin.omeragic@virginpulse.com>
2025-02-17 12:06:04 +0100

    Updated weather service to create http client manually to bypass intereceptors

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,35 +1,45 @@
-import { inject, Injectable, signal } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { of } from "rxjs";
+import { Injectable, signal } from "@angular/core";
+import { HttpBackend, HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class WeatherService {
-  private url = 'https://api.openweathermap.org/data/2.5/weather';
-  private apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+  private url = "https://api.openweathermap.org/data/2.5/weather";
+  private apiKey = "f00c38e0279b7bc85480c3fe775d518c";
+  private httpClient: HttpClient;
 
-  cityName: string = 'Tuzla';
+  cityName: string = "Tuzla";
   weather = signal("");
   iconUrl = signal("");
-  loaded = false;
+  loaded = signal(false);
 
-  constructor(private httpClient: HttpClient) {
-
+  constructor(private httpBackend: HttpBackend) {
+    /**
+     * Construct http client to prevent using interceptors
+     */
+    this.httpClient = new HttpClient(this.httpBackend);
   }
 
   getWeather() {
 
-    if (this.loaded) {
-      return of(true)
+    if (this.loaded()) {
+      return of(true);
     }
 
-    const fullUrl = `${this.url}?q=${this.cityName}&appid=${this.apiKey}&units=metric`;
+    return new Observable<boolean>((observer) => {
+      const fullUrl = `${this.url}?q=${this.cityName}&appid=${this.apiKey}&units=metric`;
 
-    return this.httpClient.get(fullUrl).subscribe((data: any) => {
+      return this.httpClient.get(fullUrl).subscribe((data: any) => {
+        const weather = data.weather[0];
+        this.weather.set(weather.main);
+        this.iconUrl.set(
+          `https://openweathermap.org/img/w/${weather.icon}.png`
+        );
 
-      const weather = data.weather[0];
-
-      this.weather.set(weather.main);
-      this.iconUrl.set(`https://openweathermap.org/img/w/${weather.icon}.png`);
+        this.loaded.set(true);
+        observer.next(true);
+        observer.complete();
+      });
     });
   }
 }
