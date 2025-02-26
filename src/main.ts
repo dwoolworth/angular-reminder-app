3e301a9ce71d6bbe612fc0f196d3871a2e97a32f
Edin Omeragic <edin.omeragic@virginpulse.com>
2025-02-26 20:36:39 +0100

    Adding analyze command build and analyze bundle sizes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { onCLS, onINP, onLCP} from 'web-vitals';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+onCLS((metric) => { console.log(metric) });
+onINP((metric) => { console.log(metric) });
+onLCP((metric) => { console.log(metric) });
